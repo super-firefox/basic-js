3bda2189457a79deb9070fdb589005e53c0d4abc
Ihor RSS <superfirfox@gmail.com>
2022-05-02 00:07:54 +0300

    feat: season

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  if(arguments.length === 0) {
+    return 'Unable to determine the time of year!'
+  }
+
+  if( Object.keys(date).length > 1 || !date) {
+    throw new Error('Invalid date!');
+  }
+  
+  try {
+    let month = date.getMonth();
+
+      switch (month) {
+        case 0:
+        case 11:
+        case 1:
+          return 'winter';
+        case 3:
+        case 4:
+        case 2:
+          return 'spring';
+        case 6:
+        case 7:
+        case 5:
+          return 'summer';
+        case 9:
+        case 10:
+        case 8:
+          return 'autumn';
+      }
+  } catch (error) {
+    throw new Error('Invalid date!');
+  }
 }
 
 module.exports = {
