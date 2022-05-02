c85f4b9891eef05abca10b6dab6155ed6322c27a
Ihor RSS <superfirfox@gmail.com>
2022-05-02 22:52:55 +0300

    feat: recursive one not work

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    let count = 1;
+    for(let item of arr) {
+      if(Array.isArray(item)) {
+        count = this.calculateDepth(item) + 1;
+      }
+    }
+
+    
+    console.log('dffdf -> ', count)
+    return count;
   }
 }
 
