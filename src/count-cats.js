fa390c5f63d02f11a889f9345dca1dd9d6e9bc59
Ihor RSS <superfirfox@gmail.com>
2022-05-02 23:44:48 +0300

    fix: count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -17,10 +17,11 @@ const { NotImplementedError } = require('../extensions/index.js');
 function countCats(matrix) {
   let cats = 0;
   for(let i = 0; i < matrix.length; i++) {
-    for(let j = 0; j < matrix[i]; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
       if(matrix[i][j] === '^^') cats++;
     }
   }
+  console.log(cats)
   return cats;
 }
 
